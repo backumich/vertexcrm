b4eae2585d66fcd461cdbd3638bde84d8eabe867
sva605 <sva605@gmail.com>
2016-11-09 16:41:19 +0200

    Add dependencies

--- web/src/main/java/ua/com/vertex/context/MainContext.java
+++ web/src/main/java/ua/com/vertex/context/MainContext.java
@@ -1,5 +1,8 @@
 package ua.com.vertex.context;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
 @ComponentScan("ua.com.vertex")
 public class MainContext {
