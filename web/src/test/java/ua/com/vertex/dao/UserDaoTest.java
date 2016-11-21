ea94f266156924eaaeb49da853fd5f09eb3034c2
carbohydrates <sahnyk.evg@gmaill.com>
2016-11-21 16:35:57 +0200

    Add sout in UserDaoTest   testing Jenkins deploying on commit

--- web/src/test/java/ua/com/vertex/dao/UserDaoTest.java
+++ web/src/test/java/ua/com/vertex/dao/UserDaoTest.java
@@ -3,7 +3,7 @@ package ua.com.vertex.dao;
 public class UserDaoTest {
     @org.junit.Test
     public void getUser() throws Exception {
-        System.out.println("Getting user");
+        System.out.println("Getting user fromDB");
     }
 
     @org.junit.Test
