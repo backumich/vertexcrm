1edbdf9dab7e4e2f478f11e9c40939350743aa1c
User <masteriza@ukr.net>
2016-12-12 04:30:10 +0200

    Merge branch 'ViacheslavKlymenko' of https://github.com/backumich/vertexcrm into ViacheslavChernyshov
    # Conflicts:
    #	web/pom.xml
    #	web/src/main/java/ua/com/vertex/context/MainContext.java
    #	web/src/main/java/ua/com/vertex/dao/UserDao.java
    #	web/src/main/webapp/WEB-INF/web.xml
    #	web/src/main/webapp/home.jsp

--- web/src/test/java/ua/com/vertex/controllers/UserControllerTest.java
+++ web/src/test/java/ua/com/vertex/controllers/UserControllerTest.java
@@ -3,34 +3,35 @@ package ua.com.vertex.controllers;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.ModelAndView;
 import ua.com.vertex.beans.UserFormRegistration;
+import ua.com.vertex.logic.interfaces.RegistrationUserLogic;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
-
+@SuppressWarnings("Duplicates")
 public class UserControllerTest {
 
-    @Autowired
-    private WebApplicationContext webAppContext;
+    @Mock
+    RegistrationUserLogic registrationUserLogic;
 
-    private RegistrationController underTest;
-    //private BindingResult bindingResult;
+    @Mock
+    private ModelAndView modelAndView;
 
-//    @Mock
-//    RegistrationUserLogic registrationUserLogic;
+    @Mock
+    private BindingResult bindingResult;
 
-    private MockMvc mockMvc;
+    private RegistrationController registrationController;
 
     @Before
     public void setUp() throws Exception {
-//        this.mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
-        //MockitoAnnotations.initMocks(this);
-        //underTest = new RegistrationController(registrationUserLogic);
+        MockitoAnnotations.initMocks(this);
+        registrationController = new RegistrationController(registrationUserLogic);
     }
 
     @Test
@@ -48,57 +49,47 @@ public class UserControllerTest {
         ModelMap modelMap = modelAndView.getModelMap();
         assertTrue(modelMap.containsAttribute("userFormRegistration"));
         Assert.assertEquals(new UserFormRegistration(), modelMap.get("userFormRegistration"));
-        //Assert.assertEquals(userFormRegistration, modelMap.get("userFormRegistration"));
     }
 
     @Test
-    public void processRegistration_checkReturnViewName() throws Exception {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("registration");
-        modelAndView.addObject(new UserFormRegistration());
+    public void viewRegistrationForm_checkReturnTypeObjects() throws Exception {
+        UserFormRegistration userFormRegistration = mock(UserFormRegistration.class);
+        registrationController.processRegistration(userFormRegistration, bindingResult, modelAndView);
 
-        RegistrationController registrationController = new RegistrationController();
-
-
-//        BindingResult bindingResult = mock(BindingResult.class);
-//        RegistrationUserLogic registrationUserLogic = mock(RegistrationUserLogic.class);
-//        UserFormRegistration userFormRegistration = mock(UserFormRegistration.class);
-//        ModelAndView model = registrationController.processRegistration(userFormRegistration, bindingResult, modelAndView);
-//        Assert.assertEquals("registrationSuccess", model.getViewName());
-
-
+        Assert.assertTrue(userFormRegistration instanceof UserFormRegistration);
+        Assert.assertTrue(bindingResult instanceof BindingResult);
+        Assert.assertTrue(modelAndView instanceof ModelAndView);
     }
 
-//    @Test
-//    public void processRegistration_checkReturnModelAndModelName() throws Exception {
-//        ModelAndView modelAndView = new ModelAndView();
-//        UserFormRegistration userFormRegistration = new UserFormRegistration();
-//        modelAndView.addObject("userFormRegistration", userFormRegistration);
-//        ModelMap modelMap = modelAndView.getModelMap();
-//        Assert.assertTrue(modelMap.containsAttribute("userFormRegistration"));
-//        Assert.assertEquals(new UserFormRegistration(), modelMap.get("userFormRegistration"));
-//        //Assert.assertEquals(userFormRegistration, modelMap.get("userFormRegistration"));
-//    }
+    //------------------------
+    @Test
+    public void processRegistration_checkReturnViewName() throws Exception {
+        UserFormRegistration userFormRegistration = mock(UserFormRegistration.class);
+        ModelAndView outModelAndView = registrationController.processRegistration(userFormRegistration, bindingResult, modelAndView);
+        Assert.assertEquals("registrationSuccess", outModelAndView.getViewName());
+    }
 
+    @Test
+    public void processRegistration_checkReturnModelAndModelName() throws Exception {
+        //UserFormRegistration userFormRegistration = mock(UserFormRegistration.class);
+        UserFormRegistration userFormRegistration = new UserFormRegistration();
+        ModelAndView modelAndView = new ModelAndView();
+        ModelAndView outModelAndView;
+        modelAndView.addObject("userFormRegistration", userFormRegistration);
 
-//    @Test
-//    public void getAllCertificateByUserIdShouldReturnAppropriateString() {
-//        String result = underTest.getAllCertificatesByUserId(222, modelAndView);
-//        assertEquals("Maybe mapping for this method was changed", "user", result);
-//    }
-//
-//    @Test
-//    public void getAllCertificateByUserIdShouldInsertCertificatesIntoModel() {
-//        ArrayList<Certificate> certificates = new ArrayList<>();
-//        certificates.add(new Certificate.Builder().getInstance());
-//
-//        when(registrationUserLogic.getAllCertificatesByUserId(333)).thenReturn(certificates);
-//
-//        underTest.getAllCertificatesByUserId(333, modelAndView);
-//
-//        Object actualResult = modelAndView.asMap().get(CERTIFICATES);
-//
-//        assertNotNull(actualResult);
-//        assertEquals("Have wrong objects in modelAndView", certificates, actualResult);
-//    }
+        outModelAndView = registrationController.processRegistration(userFormRegistration, bindingResult, modelAndView);
+        ModelMap modelMap = outModelAndView.getModelMap();
+        assertTrue(modelMap.containsAttribute("userFormRegistration"));
+        Assert.assertEquals(userFormRegistration, modelMap.get("userFormRegistration"));
+    }
+
+    @Test
+    public void processRegistration_checkReturnTypeObjects() throws Exception {
+        UserFormRegistration userFormRegistration = mock(UserFormRegistration.class);
+        registrationController.processRegistration(userFormRegistration, bindingResult, modelAndView);
+
+        Assert.assertTrue(userFormRegistration instanceof UserFormRegistration);
+        Assert.assertTrue(bindingResult instanceof BindingResult);
+        Assert.assertTrue(modelAndView instanceof ModelAndView);
+    }
 }
