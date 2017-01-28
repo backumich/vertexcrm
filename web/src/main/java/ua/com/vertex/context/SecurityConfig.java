12aa1d64512b19808db262d3c6d727f0e44d6da3
sva605 <sva605@gmail.com>
2017-01-28 22:01:59 +0200

    Add modifications to spring configuration concerning utf-8 encoding problem

--- web/src/main/java/ua/com/vertex/context/SecurityConfig.java
+++ web/src/main/java/ua/com/vertex/context/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import ua.com.vertex.logic.SpringDataUserDetailsService;
 
 import static ua.com.vertex.utils.Role.ADMIN;
@@ -41,6 +43,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        http.addFilterBefore(filter, CsrfFilter.class);
+
         http
                 .authorizeRequests()
                 .antMatchers(permittedAllRequests)
