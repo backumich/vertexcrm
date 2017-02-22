8860693034474725e006d6b847441f3711a7eec7
chernyshov_a <masteriza@ukr.net>
2017-02-22 13:05:47 +0200

    - refactoring controller UserDetailsController - add send old image to controller

--- web/src/main/java/ua/com/vertex/utils/MailService.java
+++ web/src/main/java/ua/com/vertex/utils/MailService.java
@@ -4,22 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
-@Service("mailService")
+@Service
 public class MailService {
 
 //    @Autowired
 //    private MailSender mailSender;
-//
-//    public void sendMail(String from, String to, String subject, String body) {
-//        SimpleMailMessage message = new SimpleMailMessage();
-//
-//        message.setFrom(from);
-//        message.setTo(to);
-//        message.setSubject(subject);
-//        message.setText(body);
-//        mailSender.send(message);
-//    }
+
+
+    private final MailSender mailSender;
+
+    @Autowired
+    MailService(JavaMailSender javaMailSender) {
+        this.mailSender = javaMailSender;
+    }
+
+    public void sendMail(String from, String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
 }
