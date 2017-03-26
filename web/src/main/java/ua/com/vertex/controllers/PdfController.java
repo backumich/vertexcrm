0931ba3cf9b888272478bc7030b667d5d9f25ea1
sva605 <sva605@gmail.com>
2017-03-26 23:03:24 +0300

    Pdf functionality continued

--- web/src/main/java/ua/com/vertex/controllers/PdfController.java
+++ web/src/main/java/ua/com/vertex/controllers/PdfController.java
@@ -1,7 +1,13 @@
 package ua.com.vertex.controllers;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfGState;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
@@ -12,9 +18,10 @@ import ua.com.vertex.utils.LogInfo;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Controller
@@ -30,24 +37,54 @@ public class PdfController {
                             @RequestParam String courseName, @RequestParam String certificationDate,
                             HttpServletResponse response) throws IOException {
 
-        try {
-            File file = new File(createPdf(firstName, lastName, courseName, certificationDate).toString());
+        // todo: to finish
 
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        Document document = new Document(PageSize.A6.rotate());
+
+        try (InputStream inputStream = classLoader.getResourceAsStream("certificate.docx");
+             FileOutputStream outputStream = new FileOutputStream("certificate.pdf")) {
+
+            XWPFWordExtractor extractor = new XWPFWordExtractor(new XWPFDocument(inputStream));
+
+            PdfWriter writer = PdfWriter.getInstance(document, outputStream);
+            Rectangle dimensions = new Rectangle(1120, 530);
+            document.setPageSize(dimensions);
+            document.open();
+            String fileData = extractor.getText();
+
+            PdfContentByte canvas = writer.getDirectContentUnder();
+            ClassPathResource imageToSet = new ClassPathResource("sky.jpg");
+            Image image = Image.getInstance(imageToSet.getURL());
+            image.scaleAbsolute(dimensions);
+            image.setAbsolutePosition(0, 0);
+            PdfGState state = new PdfGState();
+            state.setFillOpacity(0.6f);
+            canvas.setGState(state);
+            canvas.addImage(image);
+
+            Paragraph text = new Paragraph(fileData);
+            text.setAlignment(Element.ALIGN_CENTER);
+            document.add(text);
+            document.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (document.isOpen()) {
+                document.close();
+            }
+        }
+
+        try {
+            File pdfToDownload = new File("certificate.pdf");
             response.setContentType("application/pdf");
             response.addHeader("Content-Disposition", "attachment; filename=Certificate.pdf");
-            Files.copy(Paths.get(file.getPath()), response.getOutputStream());
+            Files.copy(Paths.get(pdfToDownload.getPath()), response.getOutputStream());
             response.getOutputStream().flush();
         } catch (IOException e) {
             LOGGER.warn(logInfo.getId(), e);
         }
-    }
-
-    private Path createPdf(String firstName, String lastName, String courseName, String certificationDate) {
-        ClassPathResource file = new ClassPathResource("");
-
-        // todo
-
-        return Paths.get("");
     }
 
     @Autowired
