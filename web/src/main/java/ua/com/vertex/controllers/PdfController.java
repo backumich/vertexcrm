8e3a5904420c292d6a9a77d79b5b3d6c66281955
sva605 <sva605@gmail.com>
2017-03-27 18:04:49 +0300

    Pdf functionality is operational

--- web/src/main/java/ua/com/vertex/controllers/PdfController.java
+++ web/src/main/java/ua/com/vertex/controllers/PdfController.java
@@ -1,26 +1,18 @@
 package ua.com.vertex.controllers;
 
-import com.itextpdf.text.*;
-import com.itextpdf.text.pdf.PdfContentByte;
-import com.itextpdf.text.pdf.PdfGState;
-import com.itextpdf.text.pdf.PdfWriter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ua.com.vertex.utils.LogInfo;
+import ua.com.vertex.utils.PdfGenerator;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -28,6 +20,7 @@ import java.nio.file.Paths;
 @RequestMapping(value = "/generatePdf")
 public class PdfController {
 
+    private final PdfGenerator pdfGenerator;
     private final LogInfo logInfo;
 
     private static final Logger LOGGER = LogManager.getLogger(PdfController.class);
@@ -37,44 +30,7 @@ public class PdfController {
                             @RequestParam String courseName, @RequestParam String certificationDate,
                             HttpServletResponse response) throws IOException {
 
-        // todo: to finish
-
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        Document document = new Document(PageSize.A6.rotate());
-
-        try (InputStream inputStream = classLoader.getResourceAsStream("certificate.docx");
-             FileOutputStream outputStream = new FileOutputStream("certificate.pdf")) {
-
-            XWPFWordExtractor extractor = new XWPFWordExtractor(new XWPFDocument(inputStream));
-
-            PdfWriter writer = PdfWriter.getInstance(document, outputStream);
-            Rectangle dimensions = new Rectangle(1120, 530);
-            document.setPageSize(dimensions);
-            document.open();
-            String fileData = extractor.getText();
-
-            PdfContentByte canvas = writer.getDirectContentUnder();
-            ClassPathResource imageToSet = new ClassPathResource("sky.jpg");
-            Image image = Image.getInstance(imageToSet.getURL());
-            image.scaleAbsolute(dimensions);
-            image.setAbsolutePosition(0, 0);
-            PdfGState state = new PdfGState();
-            state.setFillOpacity(0.6f);
-            canvas.setGState(state);
-            canvas.addImage(image);
-
-            Paragraph text = new Paragraph(fileData);
-            text.setAlignment(Element.ALIGN_CENTER);
-            document.add(text);
-            document.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (document.isOpen()) {
-                document.close();
-            }
-        }
+        pdfGenerator.generatePdf(firstName, lastName, courseName, certificationDate);
 
         try {
             File pdfToDownload = new File("certificate.pdf");
@@ -88,7 +44,8 @@ public class PdfController {
     }
 
     @Autowired
-    public PdfController(LogInfo logInfo) {
+    public PdfController(PdfGenerator pdfGenerator, LogInfo logInfo) {
+        this.pdfGenerator = pdfGenerator;
         this.logInfo = logInfo;
     }
 }
---
+++ web/src/main/java/ua/com/vertex/utils/PdfGenerator.java
@@ -0,0 +1,75 @@
+package ua.com.vertex.utils;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfWriter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+@Component
+public class PdfGenerator {
+
+    private final LogInfo logInfo;
+    private static final Logger LOGGER = LogManager.getLogger(PdfGenerator.class);
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.US);
+
+    public void generatePdf(String firstName, String lastName, String courseName, String certificationDate) {
+        Document document = new Document();
+
+        try (FileOutputStream outputStream = new FileOutputStream("certificate.pdf")) {
+
+            PdfWriter writer = PdfWriter.getInstance(document, outputStream);
+            Rectangle dimensions = new Rectangle(1120, 530);
+            document.setPageSize(dimensions);
+            document.open();
+
+            PdfContentByte canvas = writer.getDirectContentUnder();
+            ClassPathResource imageToSet = new ClassPathResource("certificate.png");
+            Image image = Image.getInstance(imageToSet.getURL());
+            image.scaleAbsolute(dimensions);
+            image.setAbsolutePosition(0, 0);
+            canvas.addImage(image);
+
+            String fullName = firstName + " " + lastName;
+            String date = formatter.format(LocalDate.parse(certificationDate));
+
+            setText(writer, fullName, BaseFont.TIMES_BOLDITALIC, 35, 560, 270);
+            setText(writer, courseName, BaseFont.TIMES_ROMAN, 32, 560, 185);
+            setText(writer, date, BaseFont.TIMES_BOLD, 15, 713, 112);
+
+            document.close();
+
+        } catch (Exception e) {
+            LOGGER.warn(logInfo.getId(), e);
+        }
+    }
+
+    private void setText(PdfWriter writer, String parameter, String baseFont, int fontSize, int shiftX, int shiftY)
+            throws IOException, DocumentException {
+
+        BaseFont bf = BaseFont.createFont(baseFont, BaseFont.WINANSI, BaseFont.EMBEDDED);
+        PdfContentByte cb = writer.getDirectContent();
+
+        cb.saveState();
+        cb.beginText();
+        cb.setFontAndSize(bf, fontSize);
+        cb.showTextAligned(Element.ALIGN_CENTER, parameter, shiftX, shiftY, 0);
+        cb.endText();
+        cb.restoreState();
+    }
+
+    @Autowired
+    public PdfGenerator(LogInfo logInfo) {
+        this.logInfo = logInfo;
+    }
+}
