032fa3028e1f8caca6af8d9e89ab5004a12ca51a
sva605 <sva605@gmail.com>
2017-03-29 12:20:33 +0300

    Minor simplification in PdfDownloader

--- web/src/main/java/ua/com/vertex/utils/PdfDownloader.java
+++ web/src/main/java/ua/com/vertex/utils/PdfDownloader.java
@@ -3,7 +3,6 @@ package ua.com.vertex.utils;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -13,10 +12,9 @@ public class PdfDownloader {
 
     public int downloadPdf(String pdfFileName, HttpServletResponse response) throws IOException {
 
-        File pdfToDownload = new File(pdfFileName);
         response.setContentType("application/pdf");
         response.addHeader("Content-Disposition", "attachment; filename=certificate.pdf");
-        Files.copy(Paths.get(pdfToDownload.getPath()), response.getOutputStream());
+        Files.copy(Paths.get(pdfFileName), response.getOutputStream());
         response.getOutputStream().flush();
 
         return response.getStatus();
