c8832af9265b63a5e5fc4eeb6625884352e027e3
sva605 <sva605@gmail.com>
2017-04-05 13:36:41 +0300

    Add SessionListener

--- web/src/main/java/ua/com/vertex/context/WebAppInitializer.java
+++ web/src/main/java/ua/com/vertex/context/WebAppInitializer.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 @Configuration
@@ -32,5 +34,11 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
     protected void customizeRegistration(ServletRegistration.Dynamic registration) {
         registration.setMultipartConfig(new MultipartConfigElement("", MAX_FILE_SIZE_BYTES,
                 MAX_REQUEST_SIZE_BYTES, MAX_FILE_SIZE_BYTES));
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        servletContext.addListener(new SessionListener());
     }
 }
