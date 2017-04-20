90ab53a6fc61e5a06608d45c7ac35b5a331d2268
ViacheslavKlymenko <syavaklimenko@mail.ru>
2017-04-20 08:30:16 +0300

    In developing

--- web/src/main/java/ua/com/vertex/dao/AccountingDaoImpl.java
+++ web/src/main/java/ua/com/vertex/dao/AccountingDaoImpl.java
@@ -3,9 +3,9 @@ package ua.com.vertex.dao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
-import ua.com.vertex.beans.Course;
 import ua.com.vertex.beans.User;
 import ua.com.vertex.dao.interfaces.AccountingDaoInf;
 
@@ -16,24 +16,30 @@ import java.util.List;
 public class AccountingDaoImpl implements AccountingDaoInf {
 
     private static final Logger LOGGER = LogManager.getLogger(CourseDaoImpl.class);
+    private static final String COLUMN_USER_ID = "user_id";
+    private static final String COLUMN_USER_EMAIL = "email";
+    private static final String COLUMN_FIRST_NAME = "first_name";
+    private static final String COLUMN_LAST_NAME = "last_name";
+    private static final String COURSE_ID = "courseId";
     private final NamedParameterJdbcTemplate jdbcTemplate;
+
+
+    @Autowired
+    public AccountingDaoImpl(DataSource dataSource) {
+        this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
 
     @Override
     public List<User> getCourseUsers(int courseId) {
-        LOGGER.debug("Try select all users by course id from db.Accounting");
 
-        String query = "SELECT id, name, start, price, teacher_name, notes FROM Courses WHERE finished=0";
-        return jdbcTemplate.query(query, (resultSet, i) -> new Course.Builder()
-                .setId(resultSet.getInt(COLUMN_COURSE_ID))
-                .setName(resultSet.getString(COLUMN_COURSE_NAME))
-                .setStart(resultSet.getTimestamp(COLUMN_COURSE_START).toLocalDateTime())
-                .setPrice(resultSet.getBigDecimal(COLUMN_COURSE_PRICE))
-                .setTeacherName(resultSet.getString(COLUMN_COURSE_TEACHER_NAME))
-                .setNotes(resultSet.getString(COLUMN_COURSE_NOTES)).getInstance());
-    }
+        LOGGER.debug(String.format("Try select all users by course id = (%s), from db.Accounting", courseId));
 
-    @Autowired
-    public CourseDaoImpl(DataSource dataSource) {
-        this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+        String query = "SELECT u.user_id, u.email, u.first_name, u.last_name FROM Users u" +
+                "  INNER JOIN Accounting a ON u.user_id = a.user_id WHERE course_id = :courseId";
+        return jdbcTemplate.query(query, new MapSqlParameterSource(COURSE_ID, courseId), (resultSet, i) -> new User.Builder().
+                setUserId(resultSet.getInt(COLUMN_USER_ID)).
+                setEmail(resultSet.getString(COLUMN_USER_EMAIL)).
+                setFirstName(resultSet.getString(COLUMN_FIRST_NAME)).
+                setLastName(resultSet.getString(COLUMN_LAST_NAME)).getInstance());
     }
 }
