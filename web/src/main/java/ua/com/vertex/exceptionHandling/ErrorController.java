ac3aefcbeb5acb4d7f8ef1ce2b50b5cd755894b5
sva605 <sva605@gmail.com>
2017-06-09 21:03:42 +0300

    Refactor exception handling

---
+++ web/src/main/java/ua/com/vertex/exceptionHandling/ErrorController.java
@@ -0,0 +1,23 @@
+package ua.com.vertex.exceptionHandling;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorController {
+
+    private static final String ERROR = "error";
+
+    @RequestMapping(value = "/error")
+    public String handleError(Model model) {
+        model.addAttribute("errorMessage", "Database is temporarily unavailable");
+        return ERROR;
+    }
+
+    @RequestMapping(value = "/404")
+    public String handle404(Model model) {
+        model.addAttribute("errorMessage", "404 — page not found");
+        return ERROR;
+    }
+}
